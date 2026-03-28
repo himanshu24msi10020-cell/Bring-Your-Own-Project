ad7d2914280c11749e5770621303e131d3006c71
himanshu24msi10020-cell <himanshu.24msi10020@vitbhopal.ac.in>
2026-03-28 09:54:44 +0530

    Add files via upload

---
+++ Student record management system.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+class Student {
+    int id;
+    String name;
+    int age;
+
+    Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
